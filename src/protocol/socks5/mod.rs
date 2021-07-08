9d1f40784479cfe14966107e2e26bc6cc9bd7199
zrah <zrah@protonmail.com>
2021-07-08 23:06:37 +0800

    rm

--- src/protocol/socks5/mod.rs
+++
@@ -1,9 +0,0 @@
-use crate::error::Error;
-use std::io;
-
-pub mod acceptor;
-mod header;
-
-fn new_error<T: ToString>(message: T) -> io::Error {
-    Error::new(format!("socks: {}", message.to_string())).into()
-}
